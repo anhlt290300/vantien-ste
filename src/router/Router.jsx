a9bdd8cad8880ec696c82b7da504abdbbbded027
anhlt290300 <70874794+anhlt290300@users.noreply.github.com>
2023-11-09 22:33:28 +0700

    test2

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,6 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Outlet,
   Route,
 } from "react-router-dom";
 
@@ -24,72 +23,52 @@ import HeaderBurger from "../parts/HeaderBurger";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
+    <Route
+      path="/"
+      element={<Layout />}
+      errorElement={
+        <>
+          <Header />
+          <HeaderBurger />
+          <Page404 />
+          <Footer />
+        </>
+      }
+    >
+      <Route index element={<Home />} />
+      <Route path="/tim-kiem" element={<Search />} errorElement={<Page404 />} />
       <Route
-        path="/"
-        element={<Layout />}
-        errorElement={
-          <>
-            <Header />
-            <HeaderBurger />
-            <Page404 />
-            <Footer />
-          </>
-        }
-      >
-        <Route index element={<Home />} />
-      </Route>
+        path="/:category/:id"
+        element={<Products />}
+        errorElement={<Page404 />}
+      />
       <Route
         path="/gioi-thieu"
-        element={<Layout />}
-        errorElement={
-          <>
-            <Header />
-            <HeaderBurger />
-            <Page404 />
-            <Footer />
-          </>
-        }
-      >
-        <Route index element={<History />} />
-      </Route>
+        element={<History />}
+        errorElement={<Page404 />}
+      />
+      <Route
+        path="/tuyen-dung"
+        element={<Recruit />}
+        errorElement={<Page404 />}
+      />
+      <Route
+        path="/dich-vu"
+        element={<Services />}
+        errorElement={<Page404 />}
+      />
+      <Route
+        path="/tin-tuc-va-su-kien"
+        element={<News />}
+        errorElement={<Page404 />}
+      />
+      <Route
+        path="/san-pham/san-pham-noi-bat"
+        element={<Products />}
+        errorElement={<Page404 />}
+      />
     </Route>
   )
 );
 
 export default router;
-{
-  /* <Route
-  path="/"
-  element={<Layout />}
-  errorElement={
-    <>
-      <Header />
-      <HeaderBurger />
-      <Page404 />
-      <Footer />
-    </>
-  }
->
-  <Route index element={<History />} />
-  <Route path="/tim-kiem" element={<Search />} errorElement={<Page404 />} />
-  <Route
-    path="/:category/:id"
-    element={<Products />}
-    errorElement={<Page404 />}
-  />
-  <Route path="/gioi-thieu" element={<History />} errorElement={<Page404 />} />
-  <Route path="/tuyen-dung" element={<Recruit />} errorElement={<Page404 />} />
-  <Route path="/dich-vu" element={<Services />} errorElement={<Page404 />} />
-  <Route
-    path="/tin-tuc-va-su-kien"
-    element={<News />}
-    errorElement={<Page404 />}
-  />
-  <Route
-    path="/san-pham/san-pham-noi-bat"
-    element={<Products />}
-    errorElement={<Page404 />}
-  />
-</Route>; */
-}
