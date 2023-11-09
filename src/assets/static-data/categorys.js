4e0b2486b7aac7a52503073878945d6b2b2fa382
anhlt290300 <70874794+anhlt290300@users.noreply.github.com>
2023-11-10 01:54:14 +0700

    category

--- src/assets/static-data/categorys.js
+++ src/assets/static-data/categorys.js
@@ -4,7 +4,7 @@ const categorys = [
     title: "Kích thủy lực holmatro",
     img: "https://kichthuyluc.com.vn/public/upload/editor-upload/files/kich-thuy-luc-Holmatro-HAHC%20100%20H%205%282%29%282%29.png",
     content:
-      ", chúng tôi chuyên cung cấp các dụng cụ cầm tay và máy móc,thiết bị, vật tư sản xuất phục vụ trong ngành Công nghiệp. Là đại diện chính hãng của Kích thủy lực Homatro (Hà Lan) tại Việt Nam, chúng tôi cam kết cung cấp cho khách hàng những sản phẩm chính hãng có giá cạnh tranh nhất với chất lượng tốt nhất theo tiêu chuẩn Châu Âu.",
+      "chúng tôi chuyên cung cấp các dụng cụ cầm tay và máy móc,thiết bị, vật tư sản xuất phục vụ trong ngành Công nghiệp. Là đại diện chính hãng của Kích thủy lực Homatro (Hà Lan) tại Việt Nam, chúng tôi cam kết cung cấp cho khách hàng những sản phẩm chính hãng có giá cạnh tranh nhất với chất lượng tốt nhất theo tiêu chuẩn Châu Âu.",
   },
 ];
 
--- src/pages/Categorys.jsx
+++ src/pages/Categorys.jsx
@@ -5,7 +5,7 @@ import BreadCrumb from "../components/BreadCrumb";
 
 const Categorys = () => {
   const { title, categorys, slug, products, content, img } = useLoaderData();
-
+  console.log(categorys);
   return (
     <section>
       <Helmet title={`Danh mục - ${title}`}>
@@ -18,51 +18,101 @@ const Categorys = () => {
         />
         <section>
           <div className="w-full tablet:py-4 py-2 desktop-L:px-32 desktop:px-16 tablet:px-8 px-4">
-            <img className="w-full tablet:max-h-[30vw]" src={img} alt={title} />
-            <div className="w-full my-4 bg-white py-4 px-6">
-              <h3 className=" font-semibold desktop:text-2xl tablet:text-xl text-lg tablet:mb-4 mb-2">{title}</h3>
+            {!categorys && (
+              <img
+                className="w-full tablet:max-h-[30vw]"
+                src={img}
+                alt={title}
+              />
+            )}
+            <div className="w-full mt-4 bg-white py-4 px-6">
+              <h3 className=" font-semibold desktop:text-2xl tablet:text-xl text-lg ">
+                {title}
+              </h3>
 
-              <p className=" tablet:text-lg  text-sm leading-8">
-                <strong>
-                  Công ty TNHH dịch vụ thương mại thiết bị Vạn Tiến
-                </strong>
-                {content}
-              </p>
+              {!categorys && (
+                <p className=" tablet:text-lg  text-sm leading-8 tablet:mt-4 mt-2">
+                  <strong>
+                    Công ty TNHH dịch vụ thương mại thiết bị Vạn Tiến
+                  </strong>,&nbsp;
+                  <span>{content}</span>
+                </p>
+              )}
             </div>
           </div>
-          <div className="w-full tablet:py-4 desktop-L:px-32 desktop:px-16 tablet:px-8 px-4 ">
-            <div className="grid desktop:grid-cols-2 grid-cols-1 gap-8 bg-white px-4 py-6">
-              {products.map((item, index) => {
-                return (
-                  <div
-                    key={index}
-                    className=" border border-gray-primary tablet:grid grid-cols-11 p-4 tablet:gap-6 gap-3"
-                  >
-                    <a
-                      href={`/danh-muc/${slug}/${item.slug}`}
-                      className="col-span-5 flex items-center"
+          <div className="w-full desktop:py-4 py-2 desktop-L:px-32 desktop:px-16 tablet:px-8 px-4 ">
+            {!categorys && (
+              <div className="grid desktop:grid-cols-2 grid-cols-1 gap-8 bg-white px-4 py-6">
+                {products.map((item, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className=" border border-gray-primary tablet:grid grid-cols-11 p-4 tablet:gap-6 gap-3"
                     >
-                      <img
-                        src={item.img[0]}
-                        className=" w-full tablet:h-full"
-                        alt=""
-                      />
-                    </a>
-                    <div className="flex flex-col col-span-6 target:mt-0 mt-4">
-                      <h3 className=" font-semibold tablet:text-xl text-base tablet:mb-4 mb-2">
-                        <a
-                          href={`/danh-muc/${slug}/${item.slug}`}
-                          className=" hover:text-red-primary"
-                        >
-                          {item.title}
-                        </a>
-                      </h3>
-                      <p className="max-h-[10rem] tablet:text-base text-sm element">{item.mini_content}</p>
+                      <a
+                        href={`/danh-muc/${slug}/${item.slug}`}
+                        className="col-span-5 flex items-center"
+                      >
+                        <img
+                          src={item.img[0]}
+                          className=" w-full tablet:h-full"
+                          alt=""
+                        />
+                      </a>
+                      <div className="flex flex-col col-span-6 target:mt-0 mt-4">
+                        <h3 className=" font-semibold tablet:text-xl text-base tablet:mb-4 mb-2">
+                          <a
+                            href={`/danh-muc/${slug}/${item.slug}`}
+                            className=" hover:text-red-primary"
+                          >
+                            {item.title}
+                          </a>
+                        </h3>
+                        <p className="max-h-[10rem] tablet:text-base text-sm element">
+                          {item.mini_content}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
+                  );
+                })}
+              </div>
+            )}
+            {categorys && (
+              <div className="grid desktop:grid-cols-2 grid-cols-1 gap-8 bg-white px-4 py-6">
+                {categorys.map((item, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className=" border border-gray-primary tablet:grid grid-cols-11 p-4 tablet:gap-6 gap-3"
+                    >
+                      <a
+                        href={`/danh-muc/${item.slug}`}
+                        className="col-span-5 flex items-center"
+                      >
+                        <img
+                          src={item.img}
+                          className=" w-full tablet:h-full"
+                          alt=""
+                        />
+                      </a>
+                      <div className="flex flex-col col-span-6 target:mt-0 mt-4">
+                        <h3 className=" font-semibold tablet:text-xl text-base tablet:mb-4 mb-2">
+                          <a
+                            href={`/danh-muc/${item.slug}`}
+                            className=" hover:text-red-primary"
+                          >
+                            {item.title}
+                          </a>
+                        </h3>
+                        <p className="max-h-[10rem] tablet:text-base text-sm element">
+                          {item.content}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </section>
       </Helmet>
